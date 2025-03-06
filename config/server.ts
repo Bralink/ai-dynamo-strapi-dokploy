7eba350adf63a3d7c4432ab414e4a0fd79f81cfa
Brayan Morales <130492015+BGinga@users.noreply.github.com>
2025-03-06 09:19:49 -0600

    update
    preview

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,14 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  preview: {
+		port: 1337,
+		host: true,    
+	},
+	server: {        
+		host: true, 
+		port: 1337
+	},
   app: {
     keys: env.array('APP_KEYS'),
   },
